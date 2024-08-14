3e3567cc8d7134634dcb0777e1304750d03aa785
Admin <aftabmaner3131@gmail.com>
2024-08-14 16:35:04 +0530

    patch 3 by aftab

--- app/src/main/java/com/aftabmaner27/mysms/MainActivity.java
+++ app/src/main/java/com/aftabmaner27/mysms/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     private String mappName="SMS";
 
+    private String mFullAppName="Stock Management System ";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
